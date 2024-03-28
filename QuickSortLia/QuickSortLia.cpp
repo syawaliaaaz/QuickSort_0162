276953f66e14e8dd7307d5bb46bcb6fed2c4f73c
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-28 10:21:08 +0700

    Membuat Void Swap

--- QuickSortLia/QuickSortLia.cpp
+++ QuickSortLia/QuickSortLia.cpp
@@ -30,6 +30,15 @@ void input() {
 		cin >> arr[i];
 	}
 }
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+
+}
 
 
 
