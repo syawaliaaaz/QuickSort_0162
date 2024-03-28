bc39a08afc0d365414fb8fd3b159729d5e4f7e44
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-28 10:40:56 +0700

    Menukar Elemen di Indeks i dengan Elemen di Index j

--- QuickSortLia/QuickSortLia.cpp
+++ QuickSortLia/QuickSortLia.cpp
@@ -68,6 +68,13 @@ void q_short(int low, int high)
 			j--; //step 8
 			cmp_count++;
 		}
+		cmp_count++;
+		if (i < j) //step 9
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+		}
+	}
 
 
 
