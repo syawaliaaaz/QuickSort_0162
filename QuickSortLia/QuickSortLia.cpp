ccb9bf6453987ba6e96564357e9b94a75ecf6d57
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-28 10:59:54 +0700

    Membuat cout terakhir Pada Void Display

--- QuickSortLia/QuickSortLia.cpp
+++ QuickSortLia/QuickSortLia.cpp
@@ -96,7 +96,12 @@ void display() {
 
 	for (int i = 0; i < n; i++)
 	{
+		cout << arr[i] << " ";
+	}
 
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
 	}
 		)
 
