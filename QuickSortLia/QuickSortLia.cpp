0787b8fc03fd1a8659c47e4e381bedf388d2ce1e
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-28 10:37:34 +0700

    Mencari Element yang Kurang Dari atau sama dengan Pivot

--- QuickSortLia/QuickSortLia.cpp
+++ QuickSortLia/QuickSortLia.cpp
@@ -61,6 +61,13 @@ void q_short(int low, int high)
 			i++; //step 6
 			cmp_count++;
 		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //step 7
+		{
+			j--; //step 8
+			cmp_count++;
+		}
 
 
 
