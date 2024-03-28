68501458eab6fb05e1462edb681e73002871a204
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-28 10:32:57 +0700

    Memasukkan Looping While Untuk Mencari Element yang Lebih Besar dari Pivot

--- QuickSortLia/QuickSortLia.cpp
+++ QuickSortLia/QuickSortLia.cpp
@@ -53,6 +53,14 @@ void q_short(int low, int high)
 	i = low + 1; //step 3
 	j = high;    //step 4
 
+	while (i <= j) //step 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) //step 5
+		{
+			i++; //step 6
+			cmp_count++;
+		}
 
 
 
